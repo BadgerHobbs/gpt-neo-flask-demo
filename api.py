7908457b73127e0ea1462a7e17353d81e9a80fc9
BadgerHobbs <andyjriggs@gmail.com>
2022-06-24 23:38:21 +0100

    Various improvements

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-
+import pydantic
+from typing import Optional, List
 from happytransformer import HappyGeneration, GENSettings
 
 happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-1.3B")
@@ -12,21 +13,36 @@ CORS(app)
 def index():
     return render_template('index.html')
 
-@app.route('/api/generate')
+@app.route('/api/generate', methods=['POST'])
 def generate():
 
-    # Get request arguments
-    prompt = request.args.get('prompt')
+    class Settings(pydantic.BaseModel):
+        min_length: Optional[int]=10
+        max_length: Optional[int]=50
+        do_sample: Optional[bool]=False
+        early_stopping: Optional[bool]=False
+        num_beams: Optional[int]=1
+        temperature: Optional[float]=1.0
+        top_k: Optional[int]=50
+        no_repeat_ngram_size: Optional[int]=0
+        top_p: Optional[float]=1.0
+        bad_words: Optional[List[str]]=None
+        
+    # Get post request data
+    data = request.get_json()
 
-    settings = request.args
-    
-    # Remove prompt from settings
-    settings.pop('prompt')
+    print(f'Processing request: {data}')
+
+    # Get request arguments
+    prompt = data.get('prompt')
+
+    # Convert settings to Settings model
+    settings = Settings(**data.get('settings'))
 
     # Generate text
     text = happy_gen.generate_text(
         prompt, 
-        args=GENSettings(**settings),
+        args=GENSettings(**settings.dict()),
     ).text
 
     # Return response
